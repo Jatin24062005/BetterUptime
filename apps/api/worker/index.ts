3d320586d6c2c5453cf2703beb268e5c8844f8aa
JATIN <Jatin04072005@gmail.com>
2025-07-28 15:43:57 +0530

    new day -05 with Redisstream and Pusher

---
+++ apps/api/worker/index.ts
@@ -0,0 +1,12 @@
+import { xAck, xReadGroup } from "redisStream";
+
+const WORKER_ID= process.env.WORKER_ID;
+const REGION_ID =process.env.REGION_ID
+
+async function main(){
+    while(1){
+
+    }
+}
+
+main();
