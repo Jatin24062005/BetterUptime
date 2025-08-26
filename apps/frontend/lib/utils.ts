c3f3f626f455a84f0614db3a5d02246ba76cdc7a
JATIN <Jatin04072005@gmail.com>
2025-08-26 22:13:29 +0530

    Updating Backend Url

--- apps/frontend/lib/utils.ts
+++ apps/frontend/lib/utils.ts
@@ -5,4 +5,4 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const BACKEND_URL ="http://localhost:4000"
+export const BACKEND_URL =" https://betteruptime-backend-api.onrender.com"
